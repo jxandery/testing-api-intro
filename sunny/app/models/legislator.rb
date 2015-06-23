d8b8f17351b38bb56f50b1b3ab0505ab50114858
Jack Yeh <jack.yeh@gmail.com>
2015-06-23 10:24:34 -0600

    not sure yet

--- sunny/app/models/legislator.rb
+++ sunny/app/models/legislator.rb
@@ -1,3 +1,11 @@
 class Legislator
 
+  def self.service
+    @service ||= SunlightService.new
+  end
+
+  # the method below is the same as Legislator.find_by()
+  def self.find_by(zipcode: "")
+    service.legislators(zipcode)
+  end
 end
--- sunny/test/models/legislator_test.rb
+++ sunny/test/models/legislator_test.rb
@@ -1,6 +1,12 @@
 class LegislatorTest < ActiveSupport::TestCase
-  test 'it exists' do
-    assert Legislator
+  test 'it finds legislators by zipcode' do
+    VCR.use_cassette("legislator#find_by_zipcode") do
+      results = Legislator.find_by(zipcode: '80202')
+
+      assert_equal 3, results.count
+      assert_equal 'Cory', results.first_name
+      assert_equal 'Gardner', results.last_name
+    end
   end
 
 end
