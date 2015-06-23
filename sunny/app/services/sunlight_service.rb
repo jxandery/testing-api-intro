248ee608beccf0f00eabf1a7d8e55234fecc3932
Jack Yeh <jack.yeh@gmail.com>
2015-06-23 09:54:02 -0600

    setup the sunlight foundation api service with Hurley: - Hurley is the tool that you use to consume any api
    - legislators is an endpoint inside the sunlight foundation api
      + creating a method to access that endpoint to retrieve data
    
    - the endpoints returns a string that then needs to be parsed in order to be useful and accessed as a hash
      + within the parse method it turns the keys into a symbol and easier to use

--- sunny/app/services/sunlight_service.rb
+++ sunny/app/services/sunlight_service.rb
@@ -1,7 +1,22 @@
 class SunlightService
+  attr_reader :connection
+
+  def initialize
+    @connection = Hurley::Client.new "http://congress.api.sunlightfoundation.com"
+    connection.query['apikey'] = 'ee45723941fd49a9a70da4cb5c634b1b'
+    # lines 5-6 is accessing the api by hitting the server with the url below
+    #http://congress.api.sunlightfoundation.com?apikey=ee45723941fd49a9a70da4cb5c634b1b
+  end
 
   def legislators(zipcode)
+    # congress.api.sunlightfoundation.com/legislators/locate?zip=80202&apikey=ee45723941fd49a9a70da4cb5c634b1b
+    parse(connection.get("legislators/locate?zip=#{zipcode}"))[:results]
+  end
 
+  private
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
 end
